b50117ef6a8d7cde9cc128dd510608a8d1513a09
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2022-10-13 14:40:02 +1100

    Added Config list of benches for Ease of Access

--- config.lua
+++ config.lua
@@ -4,7 +4,11 @@ Config = {}
 
 Config.craftingBenches = {
     {id = "pbase1", coords = vector3(97.51, 6618.9, 31.43), heading = 134.43, objExists = true, prop = 'prop_toolchest_05'},
-    {id = "testId", coords = vector3(429.16, 6478.77, 28.79), heading = 140.76, objExists = false, prop = 'gr_prop_gr_bench_04b'},
+    {id = "pspawn1", coords = vector3(429.16, 6478.77, 28.79), heading = 140.76, objExists = false, prop = 'gr_prop_gr_bench_04b'},
+}
+
+Config.benchList = {
+    base = "pbase1" or "pspawn1",
 }
 
 --[[
@@ -18,7 +22,7 @@ Config.defaultRecipes = {
         item = "radio",
         label = "Radio",
         image = "https://cfx-nui-qb-inventory/html/images/radio.png",
-        benchId = false,
+        benchId = Config.benchList.base,
         isAttachment = false,
         threshold = 0,
         points = 1,
@@ -36,7 +40,7 @@ Config.blueprintRecipes = {
         item = "advancedlockpick",
         label = "Advanced Lockpick",
         image = "https://cfx-nui-qb-inventory/html/images/advancedlockpick.png",
-        benchId = "testId",
+        benchId = Config.benchList.base,
         isAttachment = false,
         points = 1,
         components = {
